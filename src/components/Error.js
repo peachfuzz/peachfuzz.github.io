70be8fadbe14cdb15db25a966e7d51d5109a9691
Hector <hectordm65@gmail.com>
2019-03-21 23:08:39 -0700

    Fixed the 404

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import { BrowserRouter as Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import peach from "../icons/peachprincess.png";
 
 class Error extends Component {
   render() {
     return (
       <div>
-        <h1>The page {window.location.href} does not exist..</h1>\
+        <h1>The page {window.location.href} does not exist..</h1>
         <Link to="/">Go home?</Link>
+        <br />
         <img src={peach} className="peach" alt="Princess Peach from Mario" />
       </div>
     );

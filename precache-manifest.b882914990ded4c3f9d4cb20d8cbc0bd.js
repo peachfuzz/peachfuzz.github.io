603e0a29d96570630c1fd26ef42c7b3110bf3c09
Hector <hectordm65@gmail.com>
2019-03-21 22:05:23 -0700

    Updates

---
+++ precache-manifest.b882914990ded4c3f9d4cb20d8cbc0bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "3af946222d69f109b87d",
+    "url": "/static/js/main.817de060.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "3af946222d69f109b87d",
+    "url": "/static/css/main.c43dca16.chunk.css"
+  },
+  {
+    "revision": "24869ea6d33a518e7f6102d73184b830",
+    "url": "/index.html"
+  }
+];
